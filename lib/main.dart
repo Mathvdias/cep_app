cd836685a6ad68bbe8f792dae96ca87b971ae9f3
matheus <mattmvc56@gmail.com>
2023-08-03 19:20:23 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:cep_app/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CEP - Consultas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Oops!\nEm construção...🔨",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          Lottie.asset(Assets.assets_images_building_page_json),
+        ],
+      )),
+    );
+  }
+}
